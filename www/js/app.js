e4cc41fcd2575344abaa5aa51b4270a25e303a0d
Atsushi <atsushi@moongift.jp>
2021-01-29 11:24:30 +0900

    Update for handson

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,8 @@
-const applicationKey = '40706ed00ef83b9d2d107c90ebb20581341a9cbc3aae545b15df91925c1b20d8';
-const clientKey = '7c521c4005bea3a644c5bb94a17f2ea16afeff5349e29cb9b367584912ae0d9e';
+const applicationKey = 'YOUR_APPLICATION_KEY';
+const clientKey = 'YOUR_CLIENT_KEY';
 const ncmb = new NCMB(applicationKey, clientKey);
 
+// 写真をFileReaderで読み込む処理をPromise化
 async function loadPhoto(photo) {
   return new Promise(res => {
     const reader = new FileReader();
